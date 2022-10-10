09f04775f83c8e507e6d1ea64ef4b0294ace07bc
drzbraz <drz.braz@gmail.com>
2022-10-10 07:19:01 -0300

    added logo in login page

--- acmc-charity-project/src/layouts/Login/index.jsx
+++ acmc-charity-project/src/layouts/Login/index.jsx
@@ -1,18 +1,28 @@
-import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import Image from 'next/image'
+
 import * as Styles from './styles'
 export default function Login() {
   return (
     <>
       <Styles.Container>
         <Styles.LeftSide>
-          <Box>
-            <TextField id="standard-basic" label="Login" variant="standard" />
-            <TextField id="standard-basic" label="Email" variant="standard" />
-          </Box>
-          <Styles.Button></Styles.Button>
+          <Styles.Content>
+            <Styles.Input>
+              <TextField id="standard-basic" label="Email" variant="standard" />
+              <TextField id="standard-basic" label="Senha" variant="standard" style={{ marginTop: '40px' }} />
+            </Styles.Input>
+            <Styles.Button>Entrar</Styles.Button>
+          </Styles.Content>
         </Styles.LeftSide>
-        <Styles.RightSide>Welcoming to login</Styles.RightSide>
+        <Styles.RightSide>
+          <Styles.Logo>
+            <Styles.Image>
+              <Image src="/../public/assets/medical.webp" width={100} height={100} />
+            </Styles.Image>
+            <Styles.Title>Welcoming to login</Styles.Title>
+          </Styles.Logo>
+        </Styles.RightSide>
       </Styles.Container>
     </>
   )
--- acmc-charity-project/src/layouts/Login/styles.js
+++ acmc-charity-project/src/layouts/Login/styles.js
@@ -4,12 +4,14 @@ export const Container = styled.div`
   display: flex;
 `
 export const Button = styled.button`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
+  background-color: #4f77ff;
+  border-color: #4f77ff;
+  font-size: 14px;
   border-radius: 4px;
-  color: black;
-  font-weight: bold;
+  color: white;
+  padding: 12px;
+  width: 100%;
+  margin-top: 50px;
   &:hover {
     color: white;
   }
@@ -18,9 +20,33 @@ export const LeftSide = styled.div`
   background-color: rgba(255, 255, 255, 0.92);
   width: 600px;
   height: auto;
+  padding: 50px;
 `
 export const RightSide = styled.div`
   background-color: rgb(45, 56, 94, 0.9);
   width: 100%;
   height: 100vh;
+  color: white;
+  font-size: 36px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
+export const Content = styled.div`
+  margin-top: 400px;
+`
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Logo = styled.div`
+  width: 500px;
+`
+export const Image = styled.div`
+  display: flex;
+  align-items: ;
+`
+export const Title = styled.div``
